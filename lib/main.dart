257cdcfc9ca44a056ad5ce295682a7f2a7434b0c
Le Phuoc Quang <lephuocquang2003@gmail.com>
2025-08-06 08:32:47 +0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:fitness/common/colo_extension.dart';
+import 'package:fitness/view/on_boarding/on_boarding_view.dart';
+import 'package:fitness/view/on_boarding/started_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fitness 3 in 1',
+      theme: ThemeData(
+        primaryColor: TColor.primaryColor1,
+        fontFamily: "Poppins",
+      ),
+      home: const StartedView(),
+    );
+  }
+}
